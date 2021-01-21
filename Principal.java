dafa708d94827bd3c571f4db4ba6e2b77fdcb904
MateoAgudGarcia <mateo.agudelo80@eia.edu.co>
2021-01-20 22:52:22 -0500

    Try 2

--- Principal.java
+++ Principal.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class Principal{
     public static void main(String[] args) {
-        /*Scanner entry = new Scanner(System.in);
+        Scanner entry = new Scanner(System.in);
         System.out.println("Insert first number");
         int a = entry.nextInt();
         System.out.println("Insert second number");
         int b = entry.nextInt();
         
-        int c = a*b;*/
-        System.out.println("The answer is : ");
+        int c = a*b;
+        System.out.println("The answer is : "+c);
     }    
 }
    
